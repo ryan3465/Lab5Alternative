70abb3234228b03230b2384d3263822de6971445
ryan3465 <ryan3465@gmail.com>
2026-03-08 19:19:58 +0000

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+//import components
 import { StudentListComponent } from './components/student-list/student-list';
 import { WeatherComponent } from './components/weather/weather';
 
 @Component({
   selector: 'app-root',
+  //add componets to the imports
   imports: [RouterOutlet, StudentListComponent, WeatherComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
